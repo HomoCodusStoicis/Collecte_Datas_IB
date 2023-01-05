5cc155e51b79857455d2219f4fa1bf2e2c85767d
HomoCodusStoicis <florentcourtin.courtin@gmail.com>
2023-01-05 19:40:00 +0100

    Update GT_01_GetHLCS_15m_5m_1m_v2.py

--- 00 - getDataIB/GT_01_GetHLCS_15m_5m_1m_v2.py
+++ 00 - getDataIB/GT_01_GetHLCS_15m_5m_1m_v2.py
@@ -140,15 +140,100 @@ def SetupLogger():
 
 
 
+# ! [socket_declare]
+class TestClient(EClient):
+
+    def __init__(self, wrapper):
+        EClient.__init__(self, wrapper)
+        # ! [socket_declare]
+
+        # how many times a method is called to see test coverage
+        self.clntMeth2callCount = collections.defaultdict(int)
+        self.clntMeth2reqIdIdx = collections.defaultdict(lambda: -1)
+        self.reqId2nReq = collections.defaultdict(int)
+        self.setupDetectReqId()
+
+    def countReqId(self, methName, fn):
+        def countReqId_(*args, **kwargs):
+            self.clntMeth2callCount[methName] += 1
+            idx = self.clntMeth2reqIdIdx[methName]
+            if idx >= 0:
+                sign = -1 if 'cancel' in methName else 1
+                self.reqId2nReq[sign * args[idx]] += 1
+            return fn(*args, **kwargs)
+
+        return countReqId_
+
+    def setupDetectReqId(self):
+
+        methods = inspect.getmembers(EClient, inspect.isfunction)
+        for (methName, meth) in methods:
+            if methName != "send_msg":
+                # don't screw up the nice automated logging in the send_msg()
+                self.clntMeth2callCount[methName] = 0
+                # logging.debug("meth %s", name)
+                sig = inspect.signature(meth)
+                for (idx, pnameNparam) in enumerate(sig.parameters.items()):
+                    (paramName, param) = pnameNparam # @UnusedVariable
+                    if paramName == "reqId":
+                        self.clntMeth2reqIdIdx[methName] = idx
+
+                setattr(TestClient, methName, self.countReqId(methName, meth))
+
+                # print("TestClient.clntMeth2reqIdIdx", self.clntMeth2reqIdIdx)
+
+
+# ! [ewrapperimpl]
+class TestWrapper(wrapper.EWrapper):
+    # ! [ewrapperimpl]
+    def __init__(self):
+        wrapper.EWrapper.__init__(self)
+
+        self.wrapMeth2callCount = collections.defaultdict(int)
+        self.wrapMeth2reqIdIdx = collections.defaultdict(lambda: -1)
+        self.reqId2nAns = collections.defaultdict(int)
+        self.setupDetectWrapperReqId()
+
+    # TODO: see how to factor this out !!
+
+    def countWrapReqId(self, methName, fn):
+        def countWrapReqId_(*args, **kwargs):
+            self.wrapMeth2callCount[methName] += 1
+            idx = self.wrapMeth2reqIdIdx[methName]
+            if idx >= 0:
+                self.reqId2nAns[args[idx]] += 1
+            return fn(*args, **kwargs)
+
+        return countWrapReqId_
+
+    def setupDetectWrapperReqId(self):
+
+        methods = inspect.getmembers(wrapper.EWrapper, inspect.isfunction)
+        for (methName, meth) in methods:
+            self.wrapMeth2callCount[methName] = 0
+            # logging.debug("meth %s", name)
+            sig = inspect.signature(meth)
+            for (idx, pnameNparam) in enumerate(sig.parameters.items()):
+                (paramName, param) = pnameNparam # @UnusedVariable
+                # we want to count the errors as 'error' not 'answer'
+                if 'error' not in methName and paramName == "reqId":
+                    self.wrapMeth2reqIdIdx[methName] = idx
+
+            setattr(TestWrapper, methName, self.countWrapReqId(methName, meth))
+
+            # print("TestClient.wrapMeth2reqIdIdx", self.wrapMeth2reqIdIdx)
 
 # ! [socket_init]
-class IBApi(EWrapper, EClient):
+class IBApi(TestWrapper, TestClient):
 
     def __init__(self):
         print(datetime.datetime.today(), threading.current_thread().name ,"__init__")
         #TestWrapper.__init__(self)
-        EClient.__init__(self, wrapper=self)
+        TestWrapper.__init__(self)
+        TestClient.__init__(self, wrapper=self)
         # ! [socket_init]
+        
+               
         self.nKeybInt = 0
         self.started = False
         self.nextValidOrderId = None
@@ -237,6 +322,7 @@ class IBApi(EWrapper, EClient):
 
     # ! [connectack]
 
+            
     @iswrapper
     # ! [nextvalidid]
     def nextValidId(self, orderId: int):
@@ -468,6 +554,8 @@ class Bot():
         self.DateDebDt = DateInDebDt
         self.DateCourDt = DateInDebDt + datetime.timedelta(days=-1)
         self.DateFinDt = DateInFinDt
+        
+        self.fin_flux = None
     
     
         try:
